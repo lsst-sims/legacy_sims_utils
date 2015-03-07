7a1abadab0a3ff2ddf517182e85765773719433b
danielsf <scott.f.daniel@gmail.com>
2015-03-06 16:20:45 -0800

    just setup one mjd vector for the whole unit test class

--- tests/testCoordinateTransformations.py
+++ tests/testCoordinateTransformations.py
@@ -22,25 +22,43 @@ def controlEquationOfEquinoxes(mjd):
     epsilonDegrees = 23.4393 - 0.0000004*D
     return (deltaPsiHours/24.0)*2.0*numpy.pi*numpy.cos(numpy.radians(epsilonDegrees))
 
+def controlCalcGmstGast(mjd):
+    #From http://aa.usno.navy.mil/faq/docs/GAST.php Nov. 9 2013
+    mjdConv = 2400000.5
+    jd2000 = 2451545.0
+    mjd_o = math.floor(mjd)
+    jd = mjd + mjdConv
+    jd_o = mjd_o + mjdConv
+    h = 24.*(jd-jd_o)
+    d = jd - jd2000
+    d_o = jd_o - jd2000
+    t = d/36525.
+    gmst = 6.697374558 + 0.06570982441908*d_o + 1.00273790935*h + 0.000026*t**2
+    gast = gmst + utils.equationOfEquinoxes(mjd)
+    gmst %= 24.
+    gast %= 24.
+    return {'GMST':gmst, 'GAST':gast}
 
 class testCoordinateTransformations(unittest.TestCase):
 
-    def testEquationOfEquinoxes(self):
-        tolerance = 1.0e-5
+    def setUp(self):
         numpy.random.seed(32)
         ntests = 100
-        mjd = 57087 - 1000.0*(numpy.random.sample(ntests))
+        self.mjd = 57087.0 - 1000.0*(numpy.random.sample(ntests)-0.5)
+        self.tolerance = 1.0e-5
+
+    def testEquationOfEquinoxes(self):
 
         #test vectorized version
-        control = controlEquationOfEquinoxes(mjd)
-        test = utils.equationOfEquinoxes(mjd)
-        self.assertTrue(numpy.abs(test-control).max() < tolerance)
+        control = controlEquationOfEquinoxes(self.mjd)
+        test = utils.equationOfEquinoxes(self.mjd)
+        self.assertTrue(numpy.abs(test-control).max() < self.tolerance)
 
         #test non-vectorized version
-        for mm in mjd:
+        for mm in self.mjd:
             control = controlEquationOfEquinoxes(mm)
             test = utils.equationOfEquinoxes(mm)
-            self.assertTrue(numpy.abs(test-control) < tolerance)
+            self.assertTrue(numpy.abs(test-control) < self.tolerance)
 
 def suite():
     """Returns a suite containing all the test cases in this module."""
