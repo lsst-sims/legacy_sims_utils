cacf78264129ba1f23d7b42e533e6cd00e64efe0
danielsf <scott.f.daniel@gmail.com>
2015-12-14 16:36:22 -0800

    added unit test to validate utc-to-tai conversion based on data generated directly with erfa

--- tests/testModifiedJulianDate.py
+++ tests/testModifiedJulianDate.py
@@ -1,9 +1,11 @@
 import unittest
 import warnings
 import numpy as np
+import os
 import lsst.utils.tests as utilsTests
 
 import lsst.sims.utils as utils
+from lsst.utils import getPackageDir
 from lsst.sims.utils import ModifiedJulianDate
 
 class MjdTest(unittest.TestCase):
@@ -13,6 +15,31 @@ class MjdTest(unittest.TestCase):
     The 'by hand' transformations will have been tested by
     testTimeTransformations.py
     """
+
+    def test_tai_from_utc(self):
+        """
+        Load a table of UTC vs. TAI (as JD) generated directly
+        with ERFA.  Verify our ModifiedJulianDate wrapper against
+        this data.  This is mostly so that we can catch any major
+        API changes in astropy.
+        """
+
+        file_name = os.path.join(getPackageDir('sims_utils'), 'tests')
+        file_name = os.path.join(file_name, 'testData', 'utc_tai_comparison_data.txt')
+
+        dtype = np.dtype([('utc', np.float), ('tai', np.float)])
+        data = np.genfromtxt(file_name, dtype=dtype)
+
+        for uu, tt in zip(data['utc']-2400000.5, data['tai']-2400000.5):
+            mjd = ModifiedJulianDate(UTC=uu)
+            dd_sec = np.abs(mjd.TAI-tt)*86400.0
+            self.assertLess(dd_sec, 5.0e-5)
+            self.assertAlmostEqual(mjd.UTC, uu, 15)
+            mjd = ModifiedJulianDate(TAI=tt)
+            dd_sec = np.abs(mjd.UTC-uu)*86400.0
+            self.assertLess(dd_sec, 5.0e-5)
+            self.assertAlmostEqual(mjd.TAI, tt, 15)
+
 
     def test_eq(self):
         mjd1 = ModifiedJulianDate(TAI=43000.0)
