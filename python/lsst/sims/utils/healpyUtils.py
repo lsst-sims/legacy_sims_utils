f8feb1656300ac3ea67522e394d4d86c1ab74bcf
Peter Yoachim <yoachim@uw.edu>
2016-02-09 10:21:25 -0800

    make degrees and radians versions.

--- python/lsst/sims/utils/healpyUtils.py
+++ python/lsst/sims/utils/healpyUtils.py
@@ -1,10 +1,10 @@
 import numpy as np
 import healpy as hp
 
-__all__ = ['hpid2RaDec', 'raDec2Hpid', 'healbin']
+__all__ = ['hpid2RaDec', 'raDec2Hpid', 'healbin', '_hpid2RaDec', '_raDec2Hpid', '_healbin']
 
 
-def hpid2RaDec(nside, hpids):
+def _hpid2RaDec(nside, hpids):
     """
     Correct for healpy being silly and running dec from 0-180.
 
@@ -30,7 +30,29 @@ def hpid2RaDec(nside, hpids):
     return raRet, decRet
 
 
-def raDec2Hpid(nside, ra, dec):
+def hpid2RaDec(nside, hpids):
+    """
+    Correct for healpy being silly and running dec from 0-180.
+
+    Parameters
+    ----------
+    nside : int
+        Must be a value of 2^N.
+    hpids : np.array
+        Array (or single value) of healpixel IDs.
+
+    Returns
+    -------
+    raRet : float (or np.array)
+        RA positions of the input healpixel IDs. In degrees.
+    decRet : float (or np.array)
+        Dec positions of the input healpixel IDs. In degrees.
+    """
+    ra, dec = _hpid2RaDec(nside, hpids)
+    return np.degrees(ra), np.degrees(dec)
+
+
+def _raDec2Hpid(nside, ra, dec):
     """
     Assign ra,dec points to the correct healpixel.
 
@@ -39,9 +61,9 @@ def raDec2Hpid(nside, ra, dec):
     nside : int
         Must be a value of 2^N.
     ra : np.array
-        RA values to assign to healpixels.
+        RA values to assign to healpixels. Radians.
     dec : np.array
-        Dec values to assign to healpixels.
+        Dec values to assign to healpixels. Radians.
 
     Returns
     -------
@@ -53,7 +75,28 @@ def raDec2Hpid(nside, ra, dec):
     return hpids
 
 
-def healbin(ra, dec, values, nside=128, reduceFunc=np.mean, dtype=float):
+def raDec2Hpid(nside, ra, dec):
+    """
+    Assign ra,dec points to the correct healpixel.
+
+    Parameters
+    ----------
+    nside : int
+        Must be a value of 2^N.
+    ra : np.array
+        RA values to assign to healpixels. Degrees.
+    dec : np.array
+        Dec values to assign to healpixels. Degrees.
+
+    Returns
+    -------
+    hpids : np.array
+        Healpixel IDs for the input positions.
+    """
+    return _raDec2Hpid(nside, np.radians(ra), np.radians(dec))
+
+
+def _healbin(ra, dec, values, nside=128, reduceFunc=np.mean, dtype=float):
     """
     Take arrays of ra's, dec's, and value and bin into healpixels. Like numpy.hexbin but for
     bins on a sphere.
@@ -61,9 +104,9 @@ def healbin(ra, dec, values, nside=128, reduceFunc=np.mean, dtype=float):
     Parameters
     ----------
     ra : np.array
-        RA positions of the data points.
+        RA positions of the data points. Radians.
     dec : np.array
-        Dec positions of the data points.
+        Dec positions of the data points. Radians
     values : np.array
         The values at each ra,dec position.
     nside : int
@@ -93,9 +136,38 @@ def healbin(ra, dec, values, nside=128, reduceFunc=np.mean, dtype=float):
 
     # Wow, I thought histogram would be faster than the loop, but this has been faster!
     for i, idx in enumerate(pixids):
-        mapVals[idx] = reduceFunc(values[left[i]:right[i]] )
+        mapVals[idx] = reduceFunc(values[left[i]:right[i]])
 
     # Change any NaNs to healpy mask value
     mapVals[np.isnan(mapVals)] = hp.UNSEEN
 
     return mapVals
+
+
+def healbin(ra, dec, values, nside=128, reduceFunc=np.mean, dtype=float):
+    """
+    Take arrays of ra's, dec's, and value and bin into healpixels. Like numpy.hexbin but for
+    bins on a sphere.
+
+    Parameters
+    ----------
+    ra : np.array
+        RA positions of the data points. Degrees.
+    dec : np.array
+        Dec positions of the data points. Degrees.
+    values : np.array
+        The values at each ra,dec position.
+    nside : int
+        Healpixel nside resolution. Must be a value of 2^N.
+    reduceFunc : function (numpy.mean)
+        A function that will return a single value given a subset of `values`.
+    dtype : dtype ('float')
+        Data type of the resulting mask
+
+    Returns
+    -------
+    mapVals : np.array
+        A numpy array that is a valid Healpixel map.
+    """
+    return _healbin(np.radians(ra), np.radians(dec), values, nside=nside,
+                    reduceFunc=reduceFunc, dtype=dtype)
--- tests/testHealUtils.py
+++ tests/testHealUtils.py
@@ -5,7 +5,20 @@ import lsst.sims.utils as utils
 
 class TestHealUtils(unittest.TestCase):
 
-    def testRaDecs(self):
+    def testRaDecsRad(self):
+        """
+        Test that the Ra Dec conversions round-trip
+        """
+
+        nside = 64
+        hpids = np.arange(hp.nside2npix(nside))
+        ra, dec = utils._hpid2RaDec(nside, hpids)
+
+        hpids_return = utils._raDec2Hpid(nside, ra, dec)
+
+        np.testing.assert_array_equal(hpids, hpids_return)
+
+    def testRaDecsDeg(self):
         """
         Test that the Ra Dec conversions round-trip
         """
@@ -18,7 +31,31 @@ class TestHealUtils(unittest.TestCase):
 
         np.testing.assert_array_equal(hpids, hpids_return)
 
-    def testBin(self):
+
+    def testBinRad(self):
+        """
+        Test that healbin returns correct values and valid healpy maps.
+        """
+
+        ra = np.zeros(3)
+        dec = np.zeros(3)
+        values = ra*0.+1.
+
+        nside = 128
+        hpid = utils._raDec2Hpid(nside, ra[0], dec[0])
+
+        map1 = utils._healbin(ra,dec,values, nside=nside)
+        assert(map1[hpid] == 1.)
+        assert(hp.maptype(map1) == 0)
+        map2 = utils._healbin(ra,dec,values, nside=nside, reduceFunc=np.sum)
+        assert(map2[hpid] == 3.)
+        assert(hp.maptype(map2) == 0)
+        map3 = utils._healbin(ra,dec,values, nside=nside, reduceFunc=np.std)
+        assert(map3[hpid] == 0.)
+        assert(hp.maptype(map3) == 0)
+
+
+    def testBinDeg(self):
         """
         Test that healbin returns correct values and valid healpy maps.
         """
