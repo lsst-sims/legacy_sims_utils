540d3d7c43e8cb5fc71a9ed5f6fae06a21f0b80f
danielsf <scott.f.daniel@gmail.com>
2018-04-12 12:27:14 -0700

    add class to generate Zernike polynomials

--- python/lsst/sims/utils/ZernikeModule.py
+++ python/lsst/sims/utils/ZernikeModule.py
@@ -1,6 +1,7 @@
 import numpy as np
+import numbers
 
-__all__ = ["_FactorialGenerator"]
+__all__ = ["_FactorialGenerator", "ZernikePolynomialGenerator"]
 
 class _FactorialGenerator(object):
 
@@ -9,6 +10,9 @@ class _FactorialGenerator(object):
         self._max_i = 1
 
     def evaluate(self, num):
+        if num<0:
+            raise RuntimeError("Cannot handle negative factorial")
+
         i_num = int(np.round(num));
         if i_num in self._values:
             return self._values[i_num]
@@ -20,3 +24,89 @@ class _FactorialGenerator(object):
 
         self._max_i = num
         return self._values[num]
+
+
+class ZernikePolynomialGenerator(object):
+
+    def __init__(self):
+        self._factorial = _FactorialGenerator()
+        self._coeffs = {}
+        self._powers = {}
+
+    def _validate_nm(self, n, m):
+        if not isinstance(n, int) and not isinstance(n, np.int64):
+            raise RuntimeError('Zernike polynomial n must be int')
+        if not isinstance(m,int) and not isinstance(m, np.int64):
+            raise RuntimeError('Zernike polynomial m must be int')
+
+        if n<0:
+            raise RuntimeError('Radial Zernike n cannot be negative')
+        if m<0:
+            raise RuntimeError('Radial Zernike m cannot be negative')
+        if n<m:
+            raise RuntimeError('Radial Zerniki n must be >= m')
+
+        n = int(n)
+        m = int(m)
+
+        return (n, m)
+
+    def _make_polynomial(self, n, m):
+
+        n, m = self._validate_nm(n, m)
+
+        # coefficients taken from
+        # https://en.wikipedia.org/wiki/Zernike_polynomials
+
+        n_coeffs = 1+(n-m)//2
+        local_coeffs = np.zeros(n_coeffs, dtype=float)
+        local_powers = np.zeros(n_coeffs, dtype=float)
+        for k in range(0, n_coeffs):
+            if k%2 == 0:
+                sgn = 1.0
+            else:
+                sgn = -1.0
+
+            num_fac = self._factorial.evaluate(n-k)
+            k_fac = self._factorial.evaluate(k)
+            d1_fac = self._factorial.evaluate(((n+m)//2)-k)
+            d2_fac = self._factorial.evaluate(((n-m)//2)-k)
+
+            local_coeffs[k] = sgn*num_fac/(k_fac*d1_fac*d2_fac)
+            local_powers[k] = n-2*k
+
+        self._coeffs[(n,m)] = local_coeffs
+        self._powers[(n,m)] = local_powers
+
+    def _evaluate_radial(self, r, n, m):
+
+        if not isinstance(r, numbers.Number):
+            raise RuntimeError("Cannot yet handle arrays of r in Zernike")
+
+        nm_tuple = self._validate_nm(n,m)
+
+        if nm_tuple[0]-nm_tuple[1] % 2 == 1:
+            return 0.0
+
+        if nm_tuple not in self._coeffs:
+            self._make_polynomial(nm_tuple[0], nm_tuple[1])
+
+        r_term = np.power(r, self._powers[nm_tuple])
+        return (self._coeffs[nm_tuple]*r_term).sum()
+
+    def evaluate(self, r, phi, n, m):
+        """
+        phi is in radians
+        """
+        radial_part = self._evaluate_radial(r, n, np.abs(m))
+        if m>=0:
+            return radial_part*np.cos(m*phi)
+        return radial_part*np.sin(m*phi)
+
+    def norm(self, n, m):
+        nm_tuple = self._validate_nm(n, np.abs(m))
+        if nm_tuple[1] == 0:
+            eps = 2.0
+        else:
+            eps = 1.0
+        return eps*np.pi/(nm_tuple[0]*2+2)
--- tests/testZernikeModule.py
+++ tests/testZernikeModule.py
@@ -1,8 +1,11 @@
 import unittest
+import numpy as np
 
 import lsst.utils.tests
 
 from lsst.sims.utils import _FactorialGenerator
+from lsst.sims.utils import ZernikePolynomialGenerator
+
 
 def setup_module(module):
     lsst.utils.tests.init()
@@ -26,6 +29,48 @@ class FactorialTestCase(unittest.TestCase):
 
         self.assertEqual(gen.evaluate(0), 1)
         self.assertEqual(gen.evaluate(1), 1)
+
+
+class ZernikeTestCase(unittest.TestCase):
+
+    longMessage = True
+
+    @classmethod
+    def setUpClass(cls):
+        cls.d_r = 0.01
+        cls.d_phi = 0.005*np.pi
+        r_grid = np.arange(0.0, 1.0, cls.d_r)
+        phi_grid = np.arange(0.0, 2.0*np.pi, cls.d_phi)
+        mesh = np.meshgrid(r_grid, phi_grid)
+        cls.r_grid = mesh[0].flatten()
+        cls.phi_grid = mesh[1].flatten()
+
+    def test_orthogonality(self):
+
+        polynomials = {}
+        z_gen = ZernikePolynomialGenerator()
+
+        for n in range(3):
+            for m in range(-n, n+1, 2):
+                vals = np.zeros(len(self.r_grid), dtype=float)
+                for ii, (rr, pp) in enumerate(zip(self.r_grid, self.phi_grid)):
+                    vals[ii] = z_gen.evaluate(rr, pp, n, m)
+                nm_tuple = (n,m)
+                polynomials[nm_tuple] = vals
+
+        p_keys = list(polynomials.keys())
+        for ii in range(len(p_keys)):
+            p1_name = p_keys[ii]
+            p1 = polynomials[p1_name]
+            integral = (p1*p1*self.r_grid*self.d_r*self.d_phi).sum()
+            normed_integral = integral/z_gen.norm(p1_name[0], p1_name[1])
+            self.assertLess(np.abs(normed_integral-1.0), 0.04)
+            for jj in range(ii+1, len(p_keys)):
+                p2_name = p_keys[jj]
+                p2 = polynomials[p2_name]
+                dot = (p1*p2*self.r_grid*self.d_r*self.d_phi).sum()
+                msg = '\n%s norm %e\n dot %e\n' % (p1_name, integral, dot)
+                self.assertLess(np.abs(dot/integral), 0.01, msg=msg)
 
 
 class MemoryTestClass(lsst.utils.tests.MemoryTestCase):
