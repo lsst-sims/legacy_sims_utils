85095449377b5937df7d51d355b5a5a39785ab78
Peter Yoachim <yoachim@uw.edu>
2019-04-17 10:51:32 -0700

    add healpix util moc2array

--- python/lsst/sims/utils/healpyUtils.py
+++ python/lsst/sims/utils/healpyUtils.py
@@ -2,10 +2,10 @@ from __future__ import division
 import numpy as np
 import healpy as hp
 
-__all__ = ['hpid2RaDec', 'raDec2Hpid', 'healbin', '_hpid2RaDec', '_raDec2Hpid', '_healbin']
+__all__ = ['hpid2RaDec', 'raDec2Hpid', 'healbin', '_hpid2RaDec', '_raDec2Hpid', '_healbin', 'moc2array']
 
 
-def _hpid2RaDec(nside, hpids):
+def _hpid2RaDec(nside, hpids, **kwargs):
     """
     Correct for healpy being silly and running dec from 0-180.
 
@@ -24,14 +24,14 @@ def _hpid2RaDec(nside, hpids):
         Dec positions of the input healpixel IDs. In radians.
     """
 
-    lat, lon = hp.pix2ang(nside, hpids)
+    lat, lon = hp.pix2ang(nside, hpids, **kwargs)
     decRet = np.pi / 2.0 - lat
     raRet = lon
 
     return raRet, decRet
 
 
-def hpid2RaDec(nside, hpids):
+def hpid2RaDec(nside, hpids, **kwargs):
     """
     Correct for healpy being silly and running dec from 0-180.
 
@@ -49,11 +49,11 @@ def hpid2RaDec(nside, hpids):
     decRet : float (or np.array)
         Dec positions of the input healpixel IDs. In degrees.
     """
-    ra, dec = _hpid2RaDec(nside, hpids)
+    ra, dec = _hpid2RaDec(nside, hpids, **kwargs)
     return np.degrees(ra), np.degrees(dec)
 
 
-def _raDec2Hpid(nside, ra, dec):
+def _raDec2Hpid(nside, ra, dec, **kwargs):
     """
     Assign ra,dec points to the correct healpixel.
 
@@ -72,11 +72,11 @@ def _raDec2Hpid(nside, ra, dec):
         Healpixel IDs for the input positions.
     """
     lat = np.pi / 2.0 - dec
-    hpids = hp.ang2pix(nside, lat, ra)
+    hpids = hp.ang2pix(nside, lat, ra, **kwargs)
     return hpids
 
 
-def raDec2Hpid(nside, ra, dec):
+def raDec2Hpid(nside, ra, dec, **kwargs):
     """
     Assign ra,dec points to the correct healpixel.
 
@@ -94,10 +94,10 @@ def raDec2Hpid(nside, ra, dec):
     hpids : np.array
         Healpixel IDs for the input positions.
     """
-    return _raDec2Hpid(nside, np.radians(ra), np.radians(dec))
+    return _raDec2Hpid(nside, np.radians(ra), np.radians(dec), **kwargs)
 
 
-def _healbin(ra, dec, values, nside=128, reduceFunc=np.mean, dtype=float):
+def _healbin(ra, dec, values, nside=128, reduceFunc=np.mean, dtype=float, fillVal=hp.UNSEEN):
     """
     Take arrays of ra's, dec's, and value and bin into healpixels. Like numpy.hexbin but for
     bins on a sphere.
@@ -139,8 +139,8 @@ def _healbin(ra, dec, values, nside=128, reduceFunc=np.mean, dtype=float):
     for i, idx in enumerate(pixids):
         mapVals[idx] = reduceFunc(values[left[i]:right[i]])
 
-    # Change any NaNs to healpy mask value
-    mapVals[np.isnan(mapVals)] = hp.UNSEEN
+    # Change any NaNs to fill value
+    mapVals[np.isnan(mapVals)] = fillVal
 
     return mapVals
 
@@ -172,3 +172,53 @@ def healbin(ra, dec, values, nside=128, reduceFunc=np.mean, dtype=float):
     """
     return _healbin(np.radians(ra), np.radians(dec), values, nside=nside,
                     reduceFunc=reduceFunc, dtype=dtype)
+
+
+def moc2array(data, uniq, nside=128, reduceFunc=np.sum, density=True):
+    """Convert a Multi-Order Coverage Map to a single nside HEALPix array. Useful
+    for converting maps output by LIGO alerts. Expect that future versions of
+    healpy or astropy will be able to replace this functionality. Note that this is
+    a convienence function that will probably degrade portions of the MOC that are
+    sampled at high resolution.
+
+    Parameters
+    ----------
+    data : np.array
+        Data values for the MOC map
+    uniq : np.array
+        The UNIQ values for the MOC map
+    nside : int (128)
+        The output map nside
+    reduceFunc : function (np.sum)
+        The function to use to combine data into single healpixels.
+    density : bool (True)
+        If True, multiplies data values by pixel area before applying reduceFunc, and divides
+        the final array by the output pixel area. Should be True if working on a probability density MOC.
+    """
+
+    orders = np.floor(np.log2(uniq / 4) / 2).astype(int)
+    npixs = (uniq - 4 * 4**orders).astype(int)
+    nsides = 2**orders
+    names = ['ra', 'dec', 'area']
+    types = [float]*len(names)
+    data_points = np.zeros(data.size, dtype=list(zip(names, types)))
+    for order in np.unique(orders):
+        good = np.where(orders == order)
+        ra, dec = _hpid2RaDec(nsides[good][0], npixs[good], nest=True)
+        data_points['ra'][good] = ra
+        data_points['dec'][good] = dec
+        data_points['area'][good] = hp.nside2pixarea(nsides[good][0])
+
+    if density:
+        tobin_data = data*data_points['area']
+    else:
+        tobin_data = data
+
+    result = _healbin(data_points['ra'], data_points['dec'], tobin_data, nside=nside,
+                      reduceFunc=reduceFunc, fillVal=0)
+
+    if density:
+        good = np.where(result != hp.UNSEEN)
+        result[good] = result[good] / hp.nside2pixarea(nside)
+
+    return result
